f7c4fe15ac74932bae89d34d61f25bcc9c563c0f
Mustafa Buğra Avcı <mbugraavci38@gmail.com>
2023-07-29 08:35:36 +0000

    Lambda Functions' Capture Lists

--- cpp_lambda_functions.cpp
+++ cpp_lambda_functions.cpp
@@ -31,5 +31,59 @@ int main() {
 
     std::cout << "Count: " << count << std::endl;  // Output: Count: 2
 
+
+    /*-----lambda functions' capture list examples---------*/
+
+    int factor1 = 10;
+
+    // Lambda function capturing 'factor' by value
+    auto multiply1 = [factor1](int x) -> int {
+        return x * factor1;
+    };
+
+    int result2 = multiply1(5);
+    std::cout << "Result: " << result2 << std::endl;
+
+    int count1 = 0;
+
+    // Lambda function capturing 'count' by reference
+    auto increment1 = [&count1]() {
+        count1++;
+    };
+
+    increment1();
+    increment1();
+
+    std::cout << "Count: " << count1 << std::endl;  // Output: Count: 2
+
+    int count2 = 0;
+
+    // Mutable lambda function capturing 'count' by reference
+    auto increment2 = [&count2]() mutable {
+        count2++;
+    };
+
+    increment2();
+    increment2();
+
+    std::cout << "Count: " << count2 << std::endl;  // Output: Count: 2
+
+    int a1 = 10;
+    int b1 = 20;
+
+    // Capture all variables in the scope by value
+    auto sum1 = [=]() -> int {
+        return a1 + b1;
+    };
+
+    std::cout << "Sum: " << sum1() << std::endl;  // Output: Sum: 30
+
+    // Capture all variables in the scope by reference
+    auto product = [&]() -> int {
+        return a1 * b1;
+    };
+
+    std::cout << "Product: " << product() << std::endl;  // Output: Product: 200
+
     return 0;
 }
